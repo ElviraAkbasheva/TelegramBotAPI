55fcd5485fe66432f71823b14d73dd4023624c1d
ElviraAkbasheva <165509920+ElviraAkbasheva@users.noreply.github.com>
2025-02-07 00:29:34 +0300

    First upload

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+
+
+namespace TelegramBotAPI
+{
+    internal class Program
+    {
+        static public void HandleUpdateStarted(string message)
+        {
+            Console.WriteLine($"Началась обработка сообщения {message}");
+        }
+        static public void HandleUpdateCompleted(string message)
+        {
+            Console.WriteLine($"Закончилась обработка сообщения {message}");
+        }
+        static async Task Main(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+            var messHandler = new UpdateHandler();
+            messHandler.OnHandleUpdateStarted += HandleUpdateStarted;
+            messHandler.OnHandleUpdateCompleted += HandleUpdateCompleted;
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = [UpdateType.Message],
+                DropPendingUpdates = true
+            };
+
+            try
+            {
+                string token = Environment.GetEnvironmentVariable("HW_TG_TOKEN");
+                var botClient = new TelegramBotClient(token);
+                botClient.StartReceiving(messHandler, receiverOptions, cts.Token);
+
+                var me = await botClient.GetMe();
+                Console.WriteLine($"{me.FirstName} запущен");
+
+                Console.WriteLine("Нажмите клавишу A для выхода");
+
+                while (true)
+                {
+                    ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                    if (keyInfo.Key == ConsoleKey.A)
+                    {
+                        messHandler.OnHandleUpdateStarted -= HandleUpdateStarted;
+                        messHandler.OnHandleUpdateCompleted -= HandleUpdateCompleted;
+                        await cts.CancelAsync();
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Информация: {me}");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ошибка в токене");
+            }
+        }
+    }
+}
+
---
+++ UpdateHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Http.Json;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+
+namespace TelegramBotAPI
+{
+    public class UpdateHandler : IUpdateHandler
+    {
+        public delegate void MessageHandler(string message);
+        public event MessageHandler OnHandleUpdateStarted;
+        public event MessageHandler OnHandleUpdateCompleted;
+        record CatFactDto(string Fact, int Length);
+        public async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, HandleErrorSource source, CancellationToken cancellationToken)
+        {
+
+        }
+        public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            string https_adress = "https://catfact.ninja/fact";
+            string message = update.Message.Text;
+            OnHandleUpdateStarted(message);
+            await botClient.SendMessage(update.Message.Chat.Id, "Сообщение успешно принято");
+            if (message == "/cat")
+            {
+                try
+                {
+                    using var client = new HttpClient();
+                    var catFact = await client.GetFromJsonAsync<CatFactDto>(https_adress, cancellationToken);
+                    await botClient.SendMessage(update.Message.Chat.Id, catFact.Fact, cancellationToken: cancellationToken);
+                }
+                catch
+                {
+                    await botClient.SendMessage(update.Message.Chat.Id, $"Ресурс {https_adress} сейчас не доступен");
+                }
+            }
+            OnHandleUpdateCompleted(message);
+        }
+    }
+}
